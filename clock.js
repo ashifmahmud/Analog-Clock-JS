1bc0497c88f589e540f761290a4b3c3cf182d7a1
ashifmahmud <ashifmahmudjoy@gmail.com>
2022-02-02 17:45:19 +0600

    second commit

--- clock.js
+++ clock.js
@@ -1,6 +1,8 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var radius = canvas.height /2;
+
+
 ctx.translate(radius,radius);
 radius = radius*0.90;
 setInterval(drawClock,1000);
@@ -56,29 +58,48 @@ function drawNumbers(){
 function drawTime(ctx,radius){
   var now = new Date();
   var hour = now.getHours();
+
   var minute = now.getMinutes();
   var second = now.getSeconds();
 
+  var hr = hour%12;
+  hr = hr.toString();
+  var min= minute.toString();
+  var sec = second.toString();
+
+  var timeDigital = hr+":"+min+":"+sec;
+
+  document.getElementById("digital").innerHTML =timeDigital;
+
+  var color = "#333";
   hour = hour%12;
 
   hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
 
-  drawHand(ctx,hour,radius*0.5,radius*0.07);
+  drawHand(ctx,hour,radius*0.5,radius*0.07,color);
 
   minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
-  drawHand(ctx,minute,radius*0.7,radius*0.05);
+  drawHand(ctx,minute,radius*0.7,radius*0.05,color);
 
+  color = "Red";
   second = (second*Math.PI/30);
-  drawHand(ctx,second,radius*0.7,radius*0.02);
+  drawHand(ctx,second,radius*0.7,radius*0.02,color);
 }
 
-function drawHand(ctx,pos,length,width){
+function drawHand(ctx,pos,length,width,color){
+
   ctx.beginPath();
   ctx.lineWidth = width;
   ctx.linecap = "round";
   ctx.moveTo(0,0);
   ctx.rotate(pos);
   ctx.lineTo(0,-length);
+  ctx.strokeStyle = color;
   ctx.stroke();
   ctx.rotate(-pos);
+
+  ctx.beginPath();
+  ctx.arc(0,0, radius*0.05, 0,2*Math.PI);
+  ctx.fillStyle = 'red';
+  ctx.fill();
 }
